28ab44dd60110e505f3e94aba2ad30885c2a42ef
Nachasin Keawwijitwarong <Skgrobot@gmail.com>
2025-11-22 14:15:53 +0700

    Operators

---
+++ python2.py
@@ -0,0 +1,54 @@
+"""
+#
+# Part : Python Operators
+# Arithmetic Operators
+#
+"""
+
+x = 4 + 5
+y = 10 - 2
+z = 3 * 2
+a = 8 / 2
+b = 10 % 3
+
+"""
+#
+# Part : Python Operators
+# Assignment Operators
+#
+"""
+
+x = 5
+x = x + 3
+x += 3
+
+"""
+#
+# Part : Python Operators
+# Comparison iOperators
+#
+"""
+
+x = 5
+y = 10
+print(x == y) #False
+print(x != y) #True
+print(x > y)  #False
+print(x < y)  #True
+print(x >= y) #False
+print(x <= y) #True
+
+"""
+#
+# Part : Python Operators
+# logical Operators
+#
+"""
+
+x = 5
+y = 10
+print((x < 10) and (y = 5)) #True
+print((x < 10) or (y < 5))  #True
+print(not(x < 10))          #False
+print((x < 10) & (y > 5))   #True
+print((x < 10) | (y < 5))   #True
