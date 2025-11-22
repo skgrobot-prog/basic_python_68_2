a8ee5a862a99d50fe3c45cb911fdda027718eb52
Nachasin Keawwijitwarong <Skgrobot@gmail.com>
2025-11-22 14:41:50 +0700

    Collection

---
+++ python3.py
@@ -0,0 +1,56 @@
+"""
+#
+# Part : Collection of Python
+# List, Tuple, Sets, Dictionariey
+#
+"""
+
+"""
+#
+# List
+#
+"""
+
+this_list = ("apple", "banana", "cherry", "apple", "cherry")
+print(this_list[1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+# Tuple
+#
+"""
+
+this_tuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(this_tuple[1])
+print(type(this_tuple))
+print(len(this_tuple))
+
+"""
+#
+# Set
+#
+"""
+
+this_set = ("apple", "banana", "cherry", "apple", "cherry")
+print(this_set[1])
+print(type(this_set))
+print(len(this_set))
+
+"""
+#
+# Dictionariey
+#
+"""
+
+this_dict = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+}
+
+print(this_dict[1])
+print(this_dict("name"))
+print(type(this_dict))
+print(len(this_dict))
