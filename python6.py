d1dcff0351408d78746d0cf4dad2b3206293162f
Nachasin Keawwijitwarong <Skgrobot@gmail.com>
2025-11-22 15:21:02 +0700

    functions

---
+++ python6.py
@@ -0,0 +1,22 @@
+"""
+#
+# Part : functions
+#
+"""
+def myFullName(firstname="Unknow, lastname="Forger):
+    fullname = firsname + "" + lastname
+    return fullname
+
+
+print(myFullName("Loid", "Forger"))
+print(myFullName("Yor", "Forger"))
+print(myFullName("Anya", "Forger"))
+print(myFullName("Bond", "Forger"))
+
+def redpotion(hp):
+    hp += 50
+    return hp
+
+cerrenthp = 100
+print("Current HP:")
+print("After using Red Potion, HP:")
