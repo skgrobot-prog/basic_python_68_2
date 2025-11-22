36663ab4a45e9e154c450986eeb9d5ebfb520fc5
Nachasin Keawwijitwarong <Skgrobot@gmail.com>
2025-11-22 13:30:24 +0700

    Varlable

---
+++ python1.py
@@ -0,0 +1,30 @@
+"""
+#
+Part: python comment
+#
+"""
+
+print("Hello, World")
+
+"""
+#
+Part: python Variable
+#
+"""
+
+x = 5
+y = "5"
+
+print(x,y)
+
+x = str(5)
+y = int(10)
+z = float(15)
+print(type(x), type(y), type(z))
+
+"""
+#
+Part: python Variable Nameing
+#
+"""
+
